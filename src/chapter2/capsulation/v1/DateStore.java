08f1aa89e9701536e9319f4f74b11dd29a93f2db
김진환 <jake.k9245@gmail.com>
2025-05-14 20:34:32 +0900

    25.05.14-캡슐화

---
+++ src/chapter2/capsulation/v1/DateStore.java
@@ -0,0 +1,16 @@
+package chapter2.capsulation.v1;
+
+// 핵시설 관리하는 개발자
+public class DateStore {
+
+    // "B" 문자열이 들어오면 핵폭발 발생 ... !!!
+    private String store;
+
+    public void setStore(String store) {
+        if (store.equals("B")) {
+            System.out.println("B가 입력되면 안됩니다.");
+        } else {
+            this.store = store;
+        }
+    }
+}
---
+++ src/chapter2/capsulation/v1/Main.java
@@ -0,0 +1,11 @@
+package chapter2.capsulation.v1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DateStore dateStre = new DateStore();
+//        dateStre.store = "B";
+        dateStre.setStore("B");
+
+    }
+}
