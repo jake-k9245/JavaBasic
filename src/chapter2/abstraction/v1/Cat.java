aefb608ce0b13aed73a63f4cd5dbe26d31d71e13
김진환 <jake.k9245@gmail.com>
2025-05-14 22:47:58 +0900

    25.05.14-추상화

---
+++ src/chapter2/abstraction/v1/Cat.java
@@ -0,0 +1,21 @@
+package chapter2.abstraction.v1;
+
+public class Cat implements Animal {
+
+    @Override
+    public void exist() {
+        System.out.println("고양이가 존재 합니다.");
+
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("야옹");
+
+    }
+
+    public void scratch() {
+        System.out.println("스크래치");
+    }
+
+}
